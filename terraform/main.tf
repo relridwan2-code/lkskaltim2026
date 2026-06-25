4309bc9a64ef6c08b94e926f8a9372ec4e69f265
relridwan2-code <relridwan2@gmail.com>
2026-06-24 20:02:01 -0700

    Create VPC and public/private subnets in Terraform
    Added VPC and subnets for application architecture.

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,22 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+# 1. Membuat VPC
 resource "aws_vpc" "lks_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = { Name = "lks-kaltim-vpc" }
+}
+
+# 2. Public Subnet (Untuk Load Balancer/Web)
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.lks_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = { Name = "Public-Subnet" }
+}
+
+# 3. Private Subnet (Untuk Database/App)
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.lks_vpc.id
+  cidr_block = "10.0.2.0/24"
+  tags = { Name = "Private-Subnet" }
 }
